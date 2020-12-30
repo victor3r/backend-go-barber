4e6268dba30ff8cc13c60bcab27bd91f49d1c226
Victor Rabelo <victorabelo1697@gmail.com>
2020-12-29 22:57:05 -0300

    :sparkles: emails through amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,19 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+  defaults: {
+    from: {
+      email: 'victorabelo1697@gmail.com',
+      name: 'Victor',
+    },
+  },
+} as IMailConfig;

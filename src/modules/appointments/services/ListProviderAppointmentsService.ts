842dae06ad0258307f0e5bc1a199e3c168b90559
Victor Rabelo <victorabelo1697@gmail.com>
2021-01-03 22:01:07 -0300

    :sparkles: configuring cache

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -5,6 +5,7 @@ import AppError from '@shared/errors/AppError';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 interface IRequest {
   provider_id: string;
@@ -21,6 +22,9 @@ export default class ListProviderAppointmentsService {
 
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({
@@ -31,6 +35,10 @@ export default class ListProviderAppointmentsService {
   }: IRequest): Promise<Appointment[]> {
     const provider = await this.usersRepository.findById(provider_id);
 
+    const cachedData = await this.cacheProvider.recover('fas');
+
+    console.log(cachedData);
+
     if (!provider) {
       throw new AppError('Provider not found.');
     }
@@ -38,6 +46,8 @@ export default class ListProviderAppointmentsService {
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
       { provider_id, day, month, year },
     );
+
+    // this.cacheProvider.save('fas', 'afskj');
 
     return appointments;
   }

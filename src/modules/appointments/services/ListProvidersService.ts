087d8c2db92c710ce2ec0cbe8e1f3e5267133044
Victor Rabelo <victorabelo1697@gmail.com>
2021-01-10 16:31:32 -0300

    :sparkles: cache list of providers

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,6 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -13,11 +14,24 @@ export default class ListProvidersService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ user_id }: IRequest): Promise<User[]> {
-    return this.usersRepository.findAllProviders({
-      exception_user_id: user_id,
-    });
+    let providers = await this.cacheProvider.recover<User[]>(
+      `providers-list:${user_id}`,
+    );
+
+    if (!providers) {
+      providers = await this.usersRepository.findAllProviders({
+        exception_user_id: user_id,
+      });
+
+      await this.cacheProvider.save(`providers-list:${user_id}`, providers);
+    }
+
+    return providers;
   }
 }
--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -10,14 +10,14 @@ export default class RedisCacheProvider implements ICacheProvider {
     this.client = new Redis(cacheConfig.config.redis);
   }
 
-  public async save(key: string, value: string): Promise<void> {
-    await this.client.set(key, value);
+  public async save(key: string, value: any): Promise<void> {
+    await this.client.set(key, JSON.stringify(value));
   }
 
-  public async recover(key: string): Promise<string | null> {
+  public async recover<T>(key: string): Promise<T | null> {
     const data = await this.client.get(key);
 
-    return data;
+    return data ? (JSON.parse(data) as T) : null;
   }
 
   public async invalidate(key: string): Promise<void> {}
